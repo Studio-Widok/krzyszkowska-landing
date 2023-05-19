b94730d8ba196e159c0db1722b9c7f3c39e6b5d6
weterynarzfred <weterynarzfred@gmail.com>
2023-05-19 21:23:57 +0200

    nav styling

--- parts/nav.php
+++ parts/nav.php
@@ -1,17 +1,19 @@
 <nav>
-  <div id="nav-logo">
-    <img src="<?= get_template_directory_uri() ?>/media/logo_wide.png"
-      alt="logo">
-  </div>
-  <div id="nav-links">
-    <?php foreach ($parts as $partKey => $part) { ?>
-      <div class="nav-link" data-scroll-target="<?= $partKey ?>">
-        <?= $part['nav_title'] ?>
-      </div>
-    <?php } ?>
-    <div class="nav-separator">|</div>
-    <div class="nav-link">eng</div>
+  <div id="nav-wrap">
+    <img id="nav-logo"
+      src="<?= get_template_directory_uri() ?>/media/logo_wide.png" alt="logo">
+    <div id="nav-links">
+      <?php foreach ($parts as $partKey => $part) { ?>
+        <div class="nav-link" data-scroll-target="<?= $partKey ?>">
+          <?= $part['nav_title'] ?>
+        </div>
+      <?php } ?>
+      <div class="nav-separator">|</div>
+      <div class="nav-link">eng</div>
+    </div>
   </div>
 </nav>
 
 <div id="burger"></div>
+
+<div id="nav-spacer"></div>
