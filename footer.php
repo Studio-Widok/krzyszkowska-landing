65394ca9bd483f2dffc8aa2a48f5c31c581963ed
weterynarzfred <weterynarzfred@gmail.com>
2023-05-23 17:12:13 +0200

    footer

--- footer.php
+++ footer.php
@@ -1,4 +1,29 @@
-<?php wp_footer();?>
+<footer>
+  <div class="column flex flex-align-center flex-justify-space-between">
+    <div class="copyrights">
+      <?= get_field('copyrights', 'options') ?>
+    </div>
+    <div class="footer-social flex">
+      <a href="<?= get_field('facebook_link', 'options') ?>" class="social-link"
+        target="_blank" rel="noopener noreferrer">
+        <img src="<?= get_template_directory_uri() ?>/media/facebook.svg"
+          alt="facebook">
+      </a>
+      <a href="<?= get_field('youtube_link', 'options') ?>" class="social-link"
+        target="_blank" rel="noopener noreferrer">
+        <img src="<?= get_template_directory_uri() ?>/media/youtube.svg"
+          alt="youtube">
+      </a>
+      <a href="<?= get_field('instagram_link', 'options') ?>"
+        class="social-link" target="_blank" rel="noopener noreferrer">
+        <img src="<?= get_template_directory_uri() ?>/media/instagram.svg"
+          alt="instagram">
+      </a>
+    </div>
+  </div>
+</footer>
+
+<?php wp_footer(); ?>
 </body>
 
 </html>
